ec5f3f3f6675b4c75246981b88482d2ef54508a4
LIFU QIAN <qianlifu12345@126.com>
2017-08-02 17:22:02 +0800

    Update jenkinsbuild.sh

--- jenkinsbuild.sh
+++ jenkinsbuild.sh
@@ -5,6 +5,7 @@ export GOPATH="/var/lib/jenkins/goworkspace";
 export GOROOT="/usr/local/bin/go";
 env;
 echo $GOPATH;
-echo $GOROOT;  
+echo $GOROOT;
+docker login --username=bob --password=mAIZUOWANG3011 reg.miz.so
 bash ./autobuild.sh build;
 bash ./autobuild.sh docker;
